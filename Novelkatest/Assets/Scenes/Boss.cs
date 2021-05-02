4ebe3ea904102c7e002493d058712effea8ddd4b
karnak1221 <karnak1221@gmail.com>
2021-05-02 19:16:03 +0500

    alpha v001

--- Novelkatest/Assets/Scenes/Boss.cs
+++ Novelkatest/Assets/Scenes/Boss.cs
@@ -1,38 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boss : MonoBehaviour
 {
-    public int counter = 0;
+    public int counter;
+
     public bool posBool = true;
     public bool go = false;
+
     public List<int> pos = new List<int>();
     public List<int> posled = new List<int>();
-    // Start is called before the first frame update
-    void Start()
+
+    public Material ErrorMaterial;
+    public Material GoogMaterial;
+    public Material BaseMaterial;
+
+    public GameObject[] AllCube;
+
+    public RuntimeAnimatorController anim;
+    public RuntimeAnimatorController goodanim;
+    public RuntimeAnimatorController badanim;
+
+    private void Update()
     {
-        
-    }
-    void Update()
-    {
-        if (posBool == true)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            pos.Clear();
-            int j;
-            for (int i = 0; i < counter;i++)
-            {
-                j = Random.Range(1, 16);
-                pos.Add(j);
-            }
-            posBool = false;
-        }
-        if (counter  == pos.Count)
-        {
-            controll();
-            counter = 0;
+            Debug.Log("Space");
+            PlayerPrefs.SetInt("round", counter);
+            Debug.Log(PlayerPrefs.GetInt("round"));
+            SceneManager.LoadScene(1);
         }
     }
+
+    public bool checkvalue(){ return pos[counter-1] == posled[counter-1] ? true : false; }
+
     public void controll()
     {
         bool identity = true;
@@ -47,8 +50,32 @@ public class Boss : MonoBehaviour
         posled.Clear();
         Debug.Log("Its resultat: " + identity);
     }
+    private void Start()
+    {
+        reset();
+    }
     public void addinlist(int i)
     {
         posled.Add(i);
+    }
+
+    public void reset()
+    {
+        if (posBool == true)
+        {
+            pos.Clear();
+            int j;
+            for (int i = 0; i < counter; i++)
+            {
+                j = Random.Range(1, 16);
+                pos.Add(j);
+            }
+            posBool = false;
+        }
+        if (counter == pos.Count)
+        {
+            controll();
+            counter = 0;
+        }
     }
 }
